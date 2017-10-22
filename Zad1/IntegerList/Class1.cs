965f5b2d38909907bb94fecadf8c9293958a3f30
Mario Gavrić <32740465+MarioGavric@users.noreply.github.com>
2017-10-22 21:49:38 +0200

    no message

---
+++ Zad1/IntegerList/Class1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    public class IntegerList : IIntegerList
+    {
+        private int[] _internalStorage { get; set; }
+        private int brojac = 0;
+
+        public IntegerList()
+        {
+            _internalStorage = new int[4];
+        }
+    
+        public IntegerList(int initialSize)
+        {
+            if (initialSize <= 0)
+            {
+                Console.WriteLine("Unesen broj ne smije biti nula ili negativan.");
+                Console.ReadLine();
+                return;
+            }
+            _internalStorage = new int[initialSize];
+        }
+
+        public void Add(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine("Broj ne smije biti 0.");
+                Console.ReadLine();
+                return;
+            }
+            if (brojac >= _internalStorage.Length)
+            {
+                var temp = new int[(_internalStorage.Length)*2];
+                Array.Copy(_internalStorage, temp, _internalStorage.Length);
+                _internalStorage = temp;
+            }
+
+            _internalStorage[brojac] = number;
+            brojac++;
+        }
+
+        public bool Remove(int item)
+        {
+            for (int i = 0; i < _internalStorage.Length; i++)
+            {
+                if (_internalStorage[i] == item)
+                {
+                    return RemoveAt(i);
+                }
+            }
+            return false;
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index >= _internalStorage.Length || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index >= brojac) return false;
+            for (int i = index; i< (_internalStorage.Length)-1; i++)
+            {
+                _internalStorage[i] = _internalStorage[i + 1];
+            }
+            _internalStorage[(_internalStorage.Length) - 1] = 0;
+            brojac--;
+            return true;
+        }
+
+        public int GetElement(int index)
+        {
+            if (index < _internalStorage.Length && index >= 0)
+            {
+                return _internalStorage[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public int IndexOf(int item)
+        {
+            for (int i = 0; i < _internalStorage.Length; i++)
+            {
+                if (_internalStorage[i] == item) return i;
+            }
+            return -1;
+
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_internalStorage, 0, _internalStorage.Length);
+            brojac = 0;
+        }
+
+        public bool Contains(int item)
+        {
+            if (IndexOf(item) >= 0) return true;
+            return false;
+        }
+
+        public int Count => brojac;
+
+    }
+
+    public interface IIntegerList
+    {
+        /// <summary >
+        /// Adds an item to the collection .
+        /// </ summary >
+
+        void Add(int item);
+        /// <summary >
+        /// Removes the first occurrence of an item from the collection .
+        /// If the item was not found , method does nothing and returns false .
+        /// </ summary >
+        bool Remove(int item);
+        /// <summary >
+        /// Removes the item at the given index in the collection .
+        /// Throws IndexOutOfRange exception if index out of range .
+        /// </ summary >
+        bool RemoveAt(int index);
+        /// <summary >
+        /// Returns the item at the given index in the collection .
+        /// Throws IndexOutOfRange exception if index out of range .
+        /// </ summary >
+        int GetElement(int index);
+        /// <summary >
+        /// Returns the index of the item in the collection .
+        /// If item is not found in the collection , method returns -1.
+        /// </ summary >
+        int IndexOf(int item);
+        /// <summary >
+        /// Readonly property . Gets the number of items contained in the collection.
+        /// </ summary >
+        int Count { get; }
+        /// <summary >
+        /// Removes all items from the collection .
+        /// </ summary >
+        void Clear();
+        /// <summary >
+        /// Determines whether the collection contains a specific value .
+        /// </ summary >
+        bool Contains(int item);
+    }
+
+}
---
+++ Zad2/Zad2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2
+{
+    class Program
+    {
+
+        public static void ListExample(IGenericList<int> listOf)
+        {
+            listOf.Add(1); // [1]
+            listOf.Add(2); // [1 ,2]
+            listOf.Add(3); // [1 ,2 ,3]
+            listOf.Add(4); // [1 ,2 ,3 ,4]
+            listOf.Add(5); // [1 ,2 ,3 ,4 ,5]
+            listOf.RemoveAt(0); // [2 ,3 ,4 ,5]
+            listOf.Remove(5); //[2 ,3 ,4]
+            Console.WriteLine(listOf.Count); // 3
+            Console.WriteLine(listOf.Remove(100)); // false
+            Console.WriteLine(listOf.RemoveAt(5)); // false
+            listOf.Clear(); // []
+            Console.WriteLine(listOf.Count); // 0
+        }
+        static void Main(string[] args)
+        {
+            IGenericList<int> list = new GenericList<int>();
+
+            ListExample(list);
+            Console.ReadLine();
+        }
+    }
+}
